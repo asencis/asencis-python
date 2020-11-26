38ff6c43a0c335537d30d5be5aec39d79e2194b7
Michael Roberts <michael@hiyield.co.uk>
2020-11-26 16:51:03 +0000

    Bugfix: Added setuptools.find_packages() call to find all sub-packages of "asencis", fixes ModuleNotFound asencis.resources etc error.

--- asencis/__init__.py
+++ asencis/__init__.py
@@ -8,7 +8,7 @@ import os
 # Michael Roberts <michael@asencis.com>
 
 __title__ = "asencis"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "asencis Ltd"
 __license__ = "MPL 2.0"
 __copyright__ = "2020 asencis, Ltd"
--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 from asencis import __version__ as pkg_version, __author__ as pkg_author, __license__ as pkg_license
 
@@ -25,35 +25,40 @@ extras_require = {
 }
 
 setup(
-  name="asencis",
-  version=pkg_version,
-  description="asencis Python driver",
-  long_description=long_description,
-  long_description_content_type="text/markdown",
-  url="https://github.com/asencis/asencis-python",
-  author=pkg_author,
-  author_email="priority@asencis.com",
-  license=pkg_license,
-  classifiers=[
-    "Development Status :: 5 - Production/Stable",
-    "Intended Audience :: Developers",
-    "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
-    "Programming Language :: Python :: 3",
-    "Programming Language :: Python :: 3.3",
-    "Programming Language :: Python :: 3.4",
-    "Programming Language :: Python :: 3.5",
-    "Programming Language :: Python :: 3.6",
-    "Programming Language :: Python :: 3.7",
-    "Programming Language :: Python :: Implementation :: PyPy",
-    "Topic :: Software Development :: Libraries :: Python Modules",
-    "Operating System :: MacOS :: MacOS X",
-    "Operating System :: Microsoft :: Windows",
-    "Operating System :: Unix",
-  ],
-  keywords="asencis",
-  packages=["asencis"],
-  install_requires=requires,
-  extras_require=extras_require,
-  tests_require=tests_requires,
-  test_suite="nose2.collector.collector",
+    name="asencis",
+    version=pkg_version,
+    description="asencis Python driver",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/asencis/asencis-python",
+    project_urls={
+        "Bug Tracker": "https://github.com/asencis/asencis-python/issues",
+        "Documentation": "https://asencis.com/documentation",
+        "Source Code": "https://github.com/asencis/asencis-python",
+    },
+    author=pkg_author,
+    author_email="priority@asencis.com",
+    license=pkg_license,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: Unix",
+    ],
+    keywords="asencis",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    install_requires=requires,
+    extras_require=extras_require,
+    tests_require=tests_requires,
+    test_suite="nose2.collector.collector",
 )
