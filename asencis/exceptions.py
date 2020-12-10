6af242cab0c84f3207d31440a81af2b6764f891c
Michael Roberts <michael@hiyield.co.uk>
2020-12-10 12:17:38 +0000

    Code Style Issue Fixes: One-line docstring should fit on one line with quotes (found 3) (D200).

--- asencis/exceptions.py
+++ asencis/exceptions.py
@@ -1,9 +1,5 @@
 class UUIDValidationException(Exception):
-    """
-    Error returned when the uuid parameter is not valid.
-    """
+    """Error returned when the uuid parameter is not valid."""
 
 class RealmResourceSpecificationException(Exception):
-    """
-    Error returned when the realm and resource parameters are not specified.
-    """
+    """Error returned when the realm and resource parameters are not specified."""
